1bd2f1dbcb93f82ecf6e0dbabc23ff128a4c216f
max <maxgrodzki@gmail.com>
2021-07-03 13:27:17 -0400

    Imported laracasts tailwind design. Replaced views with design layouts; need to make pages dynamic with our blog content

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -40,4 +40,23 @@
     </div>
     
 </body>
-</html>
+</html> -->
+
+<x-layout>
+    @include('_posts-header')
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+        <x-post-featured-card />
+
+        <div class="lg:grid lg:grid-cols-2">
+            <x-post-card />
+            <x-post-card />
+        </div>
+
+        <div class="lg:grid lg:grid-cols-3">
+            <x-post-card />
+            <x-post-card />
+            <x-post-card />
+        </div>
+    </main>
+
+</x-layout>
