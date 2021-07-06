5ab319313bcfc9038279a322fcf3555754130a06
max <maxgrodzki@gmail.com>
2021-07-06 15:51:00 -0400

    Extracted drop-down menu to a component

--- resources/views/_posts-header.blade.php
+++ resources/views/_posts-header.blade.php
@@ -14,42 +14,33 @@
         <!--  Category -->
         <div class="relative lg:inline-flex bg-gray-100 rounded-xl">
 
+            <x-dropdown>
+                {{-- anything within this tag will be inserted in the component, where it's echo'd out --}}
+                <x-slot name="trigger">
+                    <button 
+                        class="py-2 pl-3 pr-9 text-sm font-semibold w-full lg:w-32 text-left flex lg:inline-flex">
+                        {{ isset($currentCategory) ? ucwords($currentCategory->name) : 'Categories' }}
 
-            <div x-data="{ show:false }" @click.away="show = false">
-                <button @click="show = ! show"
-                    class="py-2 pl-3 pr-9 text-sm font-semibold w-full lg:w-32 text-left flex lg:inline-flex">
-                    {{ isset($currentCategory) ? ucwords($currentCategory->name) : 'Categories' }}
+                        {{-- <x-down-arrow class="absolute pointer-events-none" style="right: 12px;"/> --}}
+                        <x-icon name='down-arrow' class="absolute pointer-events-none" style="right: 12px;"/>
+                    </button>
+                </x-slot>
 
-                    <svg class="transform -rotate-90 absolute pointer-events-none" style="right: 12px;" width="22"
-                        height="22" viewBox="0 0 22 22">
-                        <g fill="none" fill-rule="evenodd">
-                            <path stroke="#000" stroke-opacity=".012" stroke-width=".5" d="M21 1v20.16H.84V1z"></path>
-                            <path fill="#222" d="M13.854 7.224l-3.847 3.856 3.847 3.856-1.184 1.184-5.04-5.04 5.04-5.04z"></path>
-                        </g>
-                    </svg>
-                </button>
-                {{-- only shows the options if the 'show' object is truthy --}}
-                <div x-show="show" class="py-2 absolute bg-gray-100 w-full mt-2 rounded-xl z-50" style="display: none">
+                {{-- only works if the route is named 'home' --}}
+                {{-- currently not working when /posts route is named home --}}
+                <x-dropdown-item href="/posts" {{-- active="request()->routeIs('home')" --}}> All </x-dropdown-item>
 
-                    <a href="/posts"
-                        class="block text-left px-3 text-sm leading-6 hover:bg-blue-400 focus:bg-blue-400 hover:text-white focus:text-white">
-                        All
-                    </a>
+                @foreach ($category as $category)
+                    <x-dropdown-item 
+                        href="/category/{{ $category->slug }}"
+                        :active="isset($currentCategory) && $currentCategory->is($category)">
+                             {{ ucwords($category->name) }} 
+                    </x-dropdown-item>
+                         {{-- can use the is() method if you're comparing ids --}}
+                         {{-- isset checks if the view has a particular value set --}}
+                @endforeach
+            </x-dropdown>
 
-                    @foreach ($category as $category)
-                        <a href="/category/{{ $category->slug }} "
-                            class="
-                             block text-left px-3 text-sm leading-6 
-                             hover:bg-blue-400 focus:bg-blue-400 hover:text-white focus:text-white
-                             {{ isset($currentCategory) && $currentCategory->is($category) ? 'bg-blue-500 text-white': '' }}"
-                             >
-                            {{ ucwords($category->name) }}
-                        </a>
-                    @endforeach
-                </div>
-
-
-            </div>
         </div>
 
         <!-- Other Filters -->
