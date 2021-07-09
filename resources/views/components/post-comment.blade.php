8cdf74ebcd9b8abad77b7abe7f0273a1c819361a
max <maxgrodzki@gmail.com>
2021-07-09 17:42:08 -0400

    Added comment section to bottom of posts

---
+++ resources/views/components/post-comment.blade.php
@@ -0,0 +1,22 @@
+<article class="flex bg-gray-100 border border-gray-600 p-6 rounded-xl space-x-4">
+    <div style="flex-shrink-0">
+        <img src="https://i.pravatar.cc/50" alt=""  class="rounded-xl w-60">
+    </div>
+
+    <div>
+        <header class="mb-4">
+            <h3 class="font-bold">John Doe</h3>
+            <p class="text-xs">
+                Posted
+                <time>8 months ago</time>
+            </p>
+        </header>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin lectus dolor, vehicula et
+            rhoncus at, eleifend sit amet diam. Aliquam aliquam viverra nisi, sit amet laoreet est
+            molestie eu. Vestibulum molestie velit gravida elit pretium mattis. Fusce ac luctus
+            felis. 
+        </p>
+    </div>
+
+</article>
