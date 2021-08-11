2563c405c6d2d9d2e4610c6b9d2ceba5abb55bc4
max <maxgrodzki@gmail.com>
2021-08-10 22:55:48 -0400

    Swapped branding from laracasts branding

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -28,7 +28,7 @@
         <nav class="md:flex md:justify-between md:items-center">
             <div>
                 <a href="/">
-                    <img src="/images/logo.svg" alt="Laracasts Logo" width="165" height="16">
+                    <img src="/images/Site-Logo.png" alt="Laracasts Logo" width="165" height="16">
                 </a>
             </div>
 
@@ -70,7 +70,7 @@
 
         <footer id="newsletter"
             class="bg-gray-100 border border-black border-opacity-5 rounded-xl text-center py-16 px-10 mt-16">
-            <img src="/images/lary-newsletter-icon.svg" alt="" class="mx-auto -mb-6" style="width: 145px;">
+            <img src="/images/Site-Logo.png" alt="" class="mx-auto mb-3" >
             <h5 class="text-3xl">Stay in touch with the latest posts</h5>
             <p class="text-sm mt-3">Promise to keep the inbox clean. No bugs.</p>
 
--- resources/views/components/post-featured-card.blade.php
+++ resources/views/components/post-featured-card.blade.php
@@ -33,7 +33,7 @@
 
             <footer class="flex justify-between items-center mt-8">
                 <div class="flex items-center text-sm">
-                    <img src="/images/lary-avatar.svg" alt="Lary avatar">
+                    <img src="https://i.pravatar.cc/60?u={{ $post->user_id }}" alt="" width="60" height="60" class="rounded-xl">
                     <div class="ml-3">
                         <h5 class="font-bold"> 
                             <a href="/posts/?author={{ $post->author->username }}">
