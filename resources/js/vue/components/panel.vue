ce21ed1443582fcdd12decbc75ce28755e536058
max <maxgrodzki@gmail.com>
2021-08-31 21:02:26 -0400

    More components recreated with vue

---
+++ resources/js/vue/components/panel.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class='border border-gray-200 p-6 rounded-xl'>
+      <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
--- resources/js/vue/test.vue
+++ resources/js/vue/test.vue
@@ -1,14 +1,6 @@
 <template>
   <div>
-    <form action="/login" method="POST">
-        
-        <form-input name="email"/>
-        <form-input name="password"/>
-        <form-button> Submit </form-button>
-        <form-textarea name="Test"/>
-        <!-- <input type="hidden" name="_token" :value="csrf"> -->
-        
-    </form>
+    <panel></panel>
   </div>
 </template>
 
@@ -16,9 +8,10 @@
 
 <script>
 import FormButton from './components/form/form-button.vue';
+import Panel from './components/panel.vue'
 import FormInput from './components/form/form-input.vue';
 import FormTextarea from './components/form/form-textarea.vue';
 export default {
-  components: { FormInput, FormButton, FormTextarea},
+  components: { FormInput, FormButton, FormTextarea, Panel},
 };
 </script>
